d027de3214e65330061c89ce76e563e5e47e4936
Martin Saizar <saizar.martin@gmail.com>
2026-01-19 01:31:21 -0300

    refactor: moved ChangeSceneAction to scene module

--- src/pedre/actions/__init__.py
+++ src/pedre/actions/__init__.py
@@ -1,6 +1,6 @@
 """Module for actions."""
 
-from pedre.actions.base import Action, ActionSequence, ChangeSceneAction, WaitForConditionAction
+from pedre.actions.base import Action, ActionSequence, WaitForConditionAction
 from pedre.actions.registry import ActionRegistry
 
-__all__ = ["Action", "ActionRegistry", "ActionSequence", "ChangeSceneAction", "WaitForConditionAction"]
+__all__ = ["Action", "ActionRegistry", "ActionSequence", "WaitForConditionAction"]
