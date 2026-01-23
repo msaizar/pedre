f37bd569bdb2990b69c5cb702f3c932af40b25fe
Martin Saizar <saizar.martin@gmail.com>
2026-01-23 19:23:03 -0300

    feat: add dialog auto close

--- src/pedre/config.py
+++ src/pedre/config.py
@@ -62,6 +62,9 @@ class GameSettings:
     inventory_box_border_width: int = 3
     inventory_background_image: str = ""
 
+    # Dialog settings
+    dialog_auto_close_duration: float = 0.5  # seconds after text fully revealed
+
     # Pluggable systems configuration (like Django's INSTALLED_APPS)
     # List of module paths to import, which will register their systems
     # Users can add custom systems by appending to this list
